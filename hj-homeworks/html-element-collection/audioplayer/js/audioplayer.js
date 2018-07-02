71facec7c93a5f43f875a934a669ee091ae08b58
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-07-02 21:04:30 +0300

    change audioplayer

--- hj-homeworks/html-element-collection/audioplayer/js/audioplayer.js
+++ hj-homeworks/html-element-collection/audioplayer/js/audioplayer.js
@@ -10,101 +10,58 @@ const backBtn = mediaplayer.getElementsByClassName('back')[0]; // Back <button c
 const nextBtn = mediaplayer.getElementsByClassName('next')[0]; // Next <button class="next">
 
 const title = mediaplayer.getElementsByClassName('title')[0]; // название песни с помощью обновления свойства title у тега <span class="title" title="LA Chill Tour"></span>.
-const mp3List = ['LA Chill Tour', 'LA Fusion Jam', 'This is it band'];
+const mp3List = ['LA Chill Tour.mp3', 'LA Fusion Jam.mp3', 'This is it band.mp3'];
 
-let trackIndex = 0;
-
-playstateBtnBtn.onclick = () => {
+// Play or Pause
+playstateBtn.onclick = () => {
     mediaplayer.classList.contains('play') ? audio.pause() : audio.play();
     mediaplayer.classList.toggle('play');
-  }
+}
 
+// Stop
 stopBtn.onclick = () => {
    if (mediaplayer.classList.contains('play')) {
-      mediaplayer.classList.remove('play');
+       mediaplayer.classList.remove('play');
     }
-
+    
     audio.pause();
     audio.currentTime = 0;
 }
 
-prevBtn.onclick = () => {
-    if (trackIndex <= 0) {
-      trackIndex = mp3List.length - 1;
-    } else {
-      trackIndex--;
-    }
-    setCurrentTrack();
-}
-
-nextBtn.onclick = () => {
-    if (trackIndex >= mp3List.length - 1) {
-      trackIndex = 0;
-    } else {
-      trackIndex++;
-    }
-    setCurrentTrack();
-}
-
-function setCurrentTrack() {
-    audio.src = `./mp3/${mp3List[trackIndex]}.mp3`;
-    title.title = mp3List[trackIndex];
+let index = 0; // счетчик
+// Change Song of mp3List
+const currentSong = () => {
+    audio.src = `./mp3/${mp3List[index]}`; // текущий трэк
+    title.title = mp3List[index]; // название текущего трэка
 
     if (mediaplayer.classList.contains('play')) {
-      audio.play()
+        audio.play();
     }
+}
+ 
+// Button Next Song
+nextBtn.onclick = () => {
+    if (index >= mp3List.length - 1) {
+        index = 0;
+    }
+    index++;
+    currentSong();
+}
+
+// Button Back Song
+backBtn.onclick = () => {
+    if (index <= 0) {
+        index = mp3List.length - 1;
+    }
+    index--;
+    currentSong();
 }
 
 
 
 
-//const player = document.getElementsByClassName('mediaplayer')[0];
-//  const audio = player.getElementsByTagName('audio')[0];
-//  const title = player.getElementsByClassName('title')[0];
-//  const playPauseBtn = player.getElementsByClassName('playstate')[0];
-//  const stopBtn = player.getElementsByClassName('stop')[0];
-//  const prevBtn = player.getElementsByClassName('back')[0];
-//  const nextBtn = player.getElementsByClassName('next')[0];
-//  const trackList = ['LA Chill Tour', 'LA Fusion Jam', 'This is it band'];
-//  let trackIndex = 0;
-//
-//  function setCurrentTrack() {
-//    audio.src = `./mp3/${trackList[trackIndex]}.mp3`;
-//    title.title = trackList[trackIndex];
-//
-//    if (player.classList.contains('play')) {
-//      audio.play()
-//    }
-//  }
-//
-//  playPauseBtn.onclick = () => {
-//    player.classList.contains('play') ? audio.pause() : audio.play();
-//    player.classList.toggle('play');
-//  };
-//
-//  stopBtn.onclick = () => {
-//    if (player.classList.contains('play')) {
-//      player.classList.remove('play');
-//    }
-//
-//    audio.pause();
-//    audio.currentTime = 0;
-//  };
-//
-//  prevBtn.onclick = () => {
-//    if (trackIndex <= 0) {
-//      trackIndex = trackList.length - 1;
-//    } else {
-//      trackIndex--;
-//    }
-//    setCurrentTrack();
-//  }
-//
-//  nextBtn.onclick = () => {
-//    if (trackIndex >= trackList.length - 1) {
-//      trackIndex = 0;
-//    } else {
-//      trackIndex++;
-//    }
-//    setCurrentTrack();
-//  }
+
+
+
+
+  
