cd54331b9063e63de65bbf01b5b35364ef326a35
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-07-10 00:06:19 +0300

    change slider[0]

--- hj-homeworks/browser/slider/js/slider.js
+++ hj-homeworks/browser/slider/js/slider.js
@@ -1,23 +1,24 @@
-'use strict';
+`use strict`;
 
 const sliderArray = [
-    './i/airmax-jump.png',
-    './i/airmax-on-foot.png',
-    './i/airmax-playground.png',
-    './i/airmax-top-view.png',
-    './i/airmax.png'
+    airmax-jump.png,
+    airmax-on-foot.png,
+    airmax-playground.png,
+    airmax-top-view.png,
+    airmax.png
 ]
 
 const slider = document.getElementById('slider');
 let cur = 0; // счетчик
 
-slider.scr = sliderArray[0]; 
+// slider.scr = sliderArray[0]; 
+slider.src = `i/${sliderArray[0]}`;
 
 let changeSlider = setInterval(() => {
 	if (cur === sliderArray.length) {
 		cur = 0;
 	}
-	slider.src = sliderArray[cur];
+	slider.src = `i/${sliderArray[cur]}`;
 	++ cur;
 }, 5000);
 
