a0de44d80601f6092ef24f1e082a79981624f0c2
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-08-15 19:40:33 +0400

    change cart.js

--- hj-homeworks/event-bubbling-capturing/shop/js/cart.js
+++ hj-homeworks/event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,10 @@
 'use strict';
+
+list.addEventListener('click', counterCart);
+
+function counterCart(event) {
+	if (event.target.classList.contains('add-to-cart')) {
+		event.preventDefault();
+		addToCart(event.target.dataset);	
+	};	
+}
