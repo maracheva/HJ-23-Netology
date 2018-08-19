d241e48084a360787c64ccfe298d77d4b14d3671
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-08-20 00:41:56 +0400

    change after

--- hj-homeworks/event-bubbling-capturing/table/js/sort.js
+++ hj-homeworks/event-bubbling-capturing/table/js/sort.js
@@ -1,10 +1,8 @@
 'use strict';
 
 function handleTableClick(event) {
-    if (event.target.tagName !== 'TH') {
-        return;
-    } else {
-        (event.target.dataset.dir == '1') ? event.target.dataset.dir = -1 : event.target.dataset.dir = 1; 
+    if (event.target.tagName === 'TH') {
+        event.target.dataset.dir = event.target.dataset.dir === '1' ? '-1': '1'; 
         event.currentTarget.dataset.sortBy = event.target.dataset.propName;
         sortTable(event.target.dataset.propName, event.target.dataset.dir);
 
