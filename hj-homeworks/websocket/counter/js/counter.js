8c7c61e96a2565cc546e845299cce9f17ff44847
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-08-22 20:35:29 +0400

    change counter.js

--- hj-homeworks/websocket/counter/js/counter.js
+++ hj-homeworks/websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const socket = new WebSocket('wss://neto-api.herokuapp.com/counter');
+const counterOutput = document.querySelector('.counter');
+const errorsOutput = document.querySelector('.errors');
+
+window.addEventListener('beforeunload', () => {
+  socket.close(1000, 'Работа закончена.');
+});
+
+socket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
+  counterOutput.textContent = data.connections;
+  errorsOutput.textContent = data.errors;
+});
