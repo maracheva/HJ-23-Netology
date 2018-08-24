74bcea387f6b78dfd6d3651a95f7139a272fe072
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-08-25 00:57:23 +0400

    change food.js

--- hj-homeworks/cors/food/js/food.js
+++ hj-homeworks/cors/food/js/food.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const urlRecipe = 'https://neto-api.herokuapp.com/food/42';
+const urlRating = 'https://neto-api.herokuapp.com/food/42/rating';
+const urlConsumers = 'https://neto-api.herokuapp.com/food/42/consumers';
+
+const pic = document.querySelector('[data-pic]');
+const title = document.querySelector('[data-title]');
+const ingredients = document.querySelector('[data-ingredients]');
+
+const rating = document.querySelector('[data-rating]');
+const star = document.querySelector('[data-star]');
+const votes = document.querySelector('[data-votes]');
+
+const consumers = document.querySelector('[data-consumers]');
+const span = document.createElement('span');
+
+function loadData(url) {
+	return new Promise((done, fail) => {
+		window['callback'] = done;
+		const script = document.createElement('script');
+		script.src = `${url}?jsonp=${'callback'}`;
+		document.body.appendChild(script);
+	});
+}
+
+function food(data) {
+	pic.style.background = `url(${data.pic}) no-repeat`;
+	title.textContent = data.title;
+	ingredients.textContent = data.ingredients.join(', ');
+
+	return urlRating;
+}
+
+function rating(data) {
+	rating.textContent = data.rating;
+	star.style.width = `${data.rating *10}%`
+	votes.textContent = `${data.votes} оценок`;
+
+	return urlConsumers;
+}
+
+function user(data) {
+	Array.from(data.consumers).forEach( element => {
+		consumers.appendChild(document.createElement('img'));
+		consumers.lastElementChild.src = element.pic;
+		consumers.lastElementChild.setAttribute('title', element.name);
+	});
+
+	consumers.appendChild(span);
+	consumers.lastElementChild.textContent = `(+${data.total})`;
+}
+
+loadData(urlRecipe)
+	.then(food)
+	.then(res => loadData(res))
+	.then(rating)
+	.then(res => loadData(res))
+	.then(user)
+	.catch(error => {
+		console.log(error);
+	});
