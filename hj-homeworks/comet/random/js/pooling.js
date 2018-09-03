c61bd1535c1d3f6ead4e09f2793406b2d419f3f0
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-09-04 00:39:07 +0300

    change

--- hj-homeworks/comet/random/js/pooling.js
+++ hj-homeworks/comet/random/js/pooling.js
@@ -1,1 +1,16 @@
 'use strict';
+
+
+//  Pooling (Частые опросы)
+const pooling = document.querySelector('.pooling')
+
+setInterval(function () {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://neto-api.herokuapp.com/comet/pooling');
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+        loadBlock(pooling, xhr.responseText);
+    })
+}, 1000);
+
