0f97fada58043af5951976feea2949930747e675
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-09-03 22:13:48 +0300

    change socket.js

--- hj-homeworks/comet/draw/js/socket.js
+++ hj-homeworks/comet/draw/js/socket.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw'); // передача текущего состояния холста
+
+ws.addEventListener('open', event => {
+	editor.addEventListener('update', sendSession);
+});
+
+ws.addEventListener('close', event => {
+	editor.removeEventListener('update', sendSession);
+});
+
+ws.addEventListener('error', error => {
+	console.error(error.data);
+})
+
+window.addEventListener('beforeunload', () => {
+	ws.close(1000, 'Сессия завершена');
+});
+
+function sendSession(event) {
+	event.canvas.toBlob(blob => ws.send(blob));
+}
