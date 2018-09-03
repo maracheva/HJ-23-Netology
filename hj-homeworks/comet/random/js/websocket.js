3e76b68a4129b227a74c12eec64d1ef8df2cd9f7
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-09-04 00:38:48 +0300

    change

--- hj-homeworks/comet/random/js/websocket.js
+++ hj-homeworks/comet/random/js/websocket.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// WebSocket (вэб-сокет)
+// Каждое новое случайное число присылается отдельным сообщением.
+const ws = new WebSocket('wss://neto-api.herokuapp.com/comet/websocket'),
+    wbs = document.querySelector('.websocket');
+
+ws.addEventListener('message', event => {
+    loadBlock(wbs, event.data);
+})
