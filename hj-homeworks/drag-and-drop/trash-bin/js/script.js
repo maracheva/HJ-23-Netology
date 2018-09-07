586ef8860d97bc13e9a821438b3904bd0b43c3a8
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-09-07 23:01:09 +0300

    add script.js

---
+++ hj-homeworks/drag-and-drop/trash-bin/js/script.js
@@ -0,0 +1,58 @@
+'use strict'
+
+let movedPiece = null; // переменная, которая будет запоминать перемещение мыши
+// let oldCoord = null;
+let posX = null, posY = null;
+
+// Перетаскивание всех элементов с классом logo
+document.addEventListener('mousedown', event => {
+    if (event.target.classList.contains('logo')) {
+        // Предотвращаем выделение текста
+        // При захвате элемента курсор должен быть в центре этого элемента.
+        posX = event.target.getBoundingClientRect().left;
+        posY = event.target.getBoundingClientRect().top;
+        // oldCoord = {
+        //     x: event.target.getBoundingClientRect().left,
+        //     y: event.target.getBoundingClientRect().top
+        // };
+        movedPiece = event.target;
+        movedPiece.classList.add('moving');
+    }
+});
+
+document.addEventListener('mousemove', event => {
+    if (movedPiece) {
+        event.preventDefault();
+        movedPiece.style.left = `${event.pageX - (movedPiece.width / 2)}px`;
+        movedPiece.style.top = `${event.pageY - (movedPiece.height / 2)}px`;
+
+    }
+    else movedPiece = null;
+});
+
+// При захвате элемента необходимо присвоить этому элементу класс moving 
+// и удалить этот класс после отпускания элемента.
+document.addEventListener('mouseup', event => {
+    if (movedPiece) {
+        movedPiece.classList.remove('moving');
+        movedPiece.style.visibility = 'hidden';
+        const trashBin = document.elementFromPoint(event.clientX, event.clientY).closest('#trash_bin');
+        movedPiece.style.visibility = 'visible';
+        if (trashBin) {
+            movedPiece.style.display = 'none'; // Удалить элемент(display: none;), если он был отпущен в области корзины, которая имеет id trash_bin
+            movedPiece = null;
+            // oldCoord = null;
+            posX = null;
+            posY = null;
+        }  else {
+            // movedPiece.style.left = `${oldCoord.x}px`;
+            // movedPiece.style.top = `${oldCoord.y}px`;
+            movedPiece.style.left = `${posX}px`;
+            movedPiece.style.top = `${posY}px`;
+            // oldCoord = null;
+            posX = null;
+            posY = null;
+            movedPiece = null;
+        }
+    }
+});
