d46a490f911e69b6fed96d740c45467cbfe7af0b
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-09-12 00:50:05 +0300

    change main.js

--- hj-homeworks/file-api/instagram-layout/js/main.js
+++ hj-homeworks/file-api/instagram-layout/js/main.js
@@ -15,18 +15,19 @@ class iLayout {
     this.registerEvents();
   }
   registerEvents() {
-    for (let key in this.layout) {
-
-      this.layout[key].addEventListener('dragover', (event) => {
+    // for (let item in this.layout) {
+    this.layout.forEach(item => {
+        
+      this.layout[item].addEventListener('dragover', (event) => {
         event.preventDefault();
         addClass('layout__item_active', event.currentTarget);
       })
 
-      this.layout[key].addEventListener('dragleave', (event) => {
+      this.layout[item].addEventListener('dragleave', (event) => {
         removeClass('layout__item_active', event.currentTarget);
       })
 
-      this.layout[key].addEventListener('drop', (event) => {
+      this.layout[item].addEventListener('drop', (event) => {
         event.preventDefault();
         event.stopPropagation();
 
@@ -37,21 +38,22 @@ class iLayout {
         }
 
         removeClass('layout__item_active', event.currentTarget);
-        const pictureItem = document.createElement('img');
-        pictureItem.src = URL.createObjectURL(event.dataTransfer.files[0]);
-        addClass('layout__image', pictureItem);
-        pictureItem.addEventListener('load', event => {
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(event.dataTransfer.files[0]);
+        addClass('layout__image', img);
+        img.addEventListener('load', event => {
           URL.revokeObjectURL(event.currentTarget.src);
         });
-        event.currentTarget.appendChild(pictureItem);
+        event.currentTarget.appendChild(img);
       })
-    }
+    });
+    // }
+
 
     this.actionButton.addEventListener('click', (event) => {
       let images = 0;
-      for (let key in this.layout) {
-
-        if (this.layout[key].firstChild && hasClass('layout__image', this.layout[key].firstChild)) {
+      for (let item in this.layout) {
+        if (this.layout[item].firstChild && hasClass('layout__image', this.layout[item].firstChild)) {
           images++;
         }
       }
@@ -60,9 +62,10 @@ class iLayout {
         event.preventDefault();
         event.stopPropagation();
         const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        
         canvas.width = this.layout.left.offsetWidth + this.layout.top.offsetWidth;
         canvas.height = this.layout.left.offsetHeight;
-        const ctx = canvas.getContext('2d');
 
         ctx.drawImage(this.layout.left.firstChild, 0, 0, this.layout.left.offsetWidth, this.layout.left.offsetHeight, 0, 0, this.layout.left.offsetWidth, this.layout.left.offsetHeight);
         ctx.drawImage(this.layout.top.firstChild, 0, 0, this.layout.top.offsetWidth, this.layout.top.offsetHeight, this.layout.left.offsetWidth, 0, this.layout.top.offsetWidth, this.layout.top.offsetHeight);
