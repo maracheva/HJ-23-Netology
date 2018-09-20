714a33f2e30fdd70bb543c51180d871a5d8b400c
Daria Maracheva <34705952+maracheva@users.noreply.github.com>
2018-09-20 03:23:51 +0300

    change script.js

--- hj-homeworks/media/photo-booth/js/script.js
+++ hj-homeworks/media/photo-booth/js/script.js
@@ -1,147 +1,167 @@
 'use strict';
 
-const app = document.querySelector('.app');
-const controls = document.querySelector('.controls');
-const takePhotoBth = controls.querySelector('#take-photo');
-const errorMessage = document.getElementById('error-message');
-const listPhoto = document.querySelector('.list');
+document.addEventListener('DOMContentLoaded', () => {
+    const app = document.querySelector('.app');
+    const controls = document.querySelector('.controls');
+    const errorMessage = document.querySelector('#error-message');
+    const takePhotoBth = document.querySelector('#take-photo');
+    const listPhoto = document.querySelector('.list');
 
-const video = document.createElement('video');
-const audio = document.createElement('audio');
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
 
-audio.src = './audio/click.mp3';
 
-// Запрашиваем доступ к веб-камере. Если доступ не предоставлен, показываем ошибку
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices
-    .getUserMedia({video: true, audio: false})
-    .then(stream => {
-      video.src = URL.createObjectURL(stream);
-      video.play();
-      app.appendChild(video);
+    // Запрашиваем доступ к веб-камере. Если доступ не предоставлен, показываем ошибку
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: false
+        })
+        .then((stream) => {
+            document.querySelector('.app').appendChild(getElem(video));
+            document.querySelector('.video').srcObject = stream;
+        })
+        .catch(err => {
+            errorMessage.style.display = 'block';
+            errorMessage.innerText = err;
+        });
 
-      controls.style.display = 'block';
-      // Обрабатываем событие на кнопке «Сделать снимок» 
-      takePhotoBth.addEventListener('click', () => getPhoto(stream));
-    })
-    .catch(error => {
-      errorMessage.textContent = `Нет доступа к камере. Ошибка ${error.name}`;
-      errorMessage.style.display = 'block';
+    // делаем фото
+    function createPhoto() {
+        const video = document.querySelector('video');
+        // Установка размеров canvas идентичных с video.
+        canvas.width = video.videoHeight;
+        canvas.height = video.videoWidth;
+
+        // копируем текущий кадр видеотега в canvas
+        ctx.drawImage(video, 0, 0);
+
+        img.childs[1].childs["0"].props.href = canvas.toDataURL();
+        img.childs["0"].props.src = canvas.toDataURL();
+
+        // обновляем картинку на странице
+        if (listPhoto.firstChild) {
+            listPhoto.insertBefore(getElem(img), listPhoto.firstChild)
+        } else {
+            listPhoto.appendChild(getElem(img));
+        }
+
+        const figcaption = document.querySelectorAll('figcaption');
+        Array.from(figcaption).forEach(item => {
+            item.addEventListener('click', getFormData);
+        })
+    }
+
+    function getFormData(event) {
+        if (event.target.parentNode === event.currentTarget.children[1]) {
+            event.currentTarget.children[1].setAttribute('href', '');
+            event.currentTarget.children[1].style.opacity = 0;
+            event.preventDefault();
+            const data = new FormData()
+            canvas.toBlob(blob => {
+                data.append('image', blob);
+                // отправка формы
+                fetch('https://neto-api.herokuapp.com/photo-booth', {
+                    method: 'post',
+                    body: data
+                })
+                .then(console.log);
+            })
+        }
+
+        if (event.target.parentNode === event.currentTarget.children[0]) {
+            event.currentTarget.children[0].setAttribute('href', '');
+            event.currentTarget.children[0].style.opacity = 0;
+        }
+
+        if (event.target.parentNode === event.currentTarget.children[2]) {
+            event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode);
+        }
+
+    }
+    
+    // функция создани DOM элемента
+    function getElem(node) {
+        const element = document.createElement(node.name);
+        if ((typeof node === 'string')) {
+            return document.createTextNode(node.toString());
+        }
+
+        if (Array.isArray(node)) {
+            return node.reduce((f, elem) => {
+                f.appendChild(getElem(elem));
+                return f;
+            }, document.createDocumentFragment());
+        }
+
+        if (node.props) {
+            Object.keys(node.props).forEach(
+                key => element.setAttribute(key, node.props[key])
+            );
+        }
+
+        if (node.childs) {
+            element.appendChild(getElem(node.childs))
+        };
+
+        return element;
+    }
+
+    //DOM elements
+    const el = (name, props, ...childs) => ({
+        name,
+        props,
+        childs
     });
-} else {
-  errorMessage.textContent = 'Ваш браузер не поддерживает mediaDevices';
-  errorMessage.style.display = 'block';
-}
+    const video = el('video', {
+        'autoplay': '',
+        'class': 'video'
+    });
+    const img = el('figure', null,
+        el('img', {
+        'src': canvas.toDataURL()
+        }),
+        el(
+        'figcaption',
+        null,
+        el(
+            'a', {
+            'href': canvas.toDataURL(),
+            'download': "snapshot.png"
+            },
+            el('i', {
+            'class': 'material-icons'
+            }, 'file_download')),
+        el(
+            'a',
+            null,
+            el('i', {
+            'class': 'material-icons'
+            }, 'file_upload')),
+        el(
+            'a',
+            null,
+            el('i', {
+            'class': 'material-icons'
+            }, 'delete'))
+        )
+    );
 
-function getPhoto(stream) {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d'); // Объект для работы с canvas.
-  let newListPhoto, dataUrl;
-  // Установка размеров canvas идентичных с video.
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
+   
+    // обработка событий
+    app.addEventListener('mouseenter', () => {
+        controls.style.display = 'block'
+    });
+    app.addEventListener('mouseleave', () => {
+        controls.style.display = ''
+    });
 
-  ctx.drawImage(video, 0, 0);
+    takePhotoBth.addEventListener('click', () => {
+        takePhotoBth.appendChild(getElem(el('audio', {
+            'src': './audio/click.mp3'
+        })))
+        takePhotoBth.getElementsByTagName('audio')[0].play();
+        createPhoto();
 
-  dataUrl = canvas.toDataURL();
-  newListPhoto = createImg(dataUrl);
-  // обрабатываем события на кнопках
-  newListPhoto.addEventListener('click', (e) => {
-    switch(e.target.textContent) {
-      // При клике на кнопке «Загрузить» отправляем выбранное фото на сервер.
-      case 'file_download':
-        e.target.style.display = 'none';
-        break;
-      // При клике на кнопке «Скачать» сохраняем выбранное фото на диске.
-      case 'file_upload':
-        fetchRequest(dataUrl, e.target);
-        break;
-      // При клике на кнопке «Удалить» удаляем фото из списка
-      case 'delete':
-        newListPhoto.parentNode.removeChild(newListPhoto);
-        break;
-    }
-  });
-
-  listPhoto.appendChild(newListPhoto);
-
-  audio.play();
-}
-
-// Шаблон создания HTML для изображения
-function createImg(imgUrl) {
-  return {
-      block: 'figure',
-      content: [{
-          block: 'img',
-          attrs: {
-              src: imgUrl
-          }
-      }, {
-          block: 'figcaption',
-          content: [{
-              block: 'a',
-              attrs: {
-                  href: imgUrl,
-                  download: 'snapshot.png'
-              },
-              content: [{
-                  block: 'i',
-                  cls: 'material-icons',
-                  content: ['file_download']
-              }]
-          }, {
-              block: 'a',
-              content: [{
-                  block: 'i',
-                  cls: 'material-icons',
-                  content: ['file_upload']
-              }]
-          }, {
-              block: 'a',
-              content: [{
-                  block: 'i',
-                  cls: 'material-icons',
-                  content: ['delete']
-              }]
-          }]
-
-      }]
-  }
-};
-
-// функция отправки формы
-function fetchRequest(imgData, target) {
-  const data = new FormData();
-  const blob = dataUriToBlob(imgData);
-
-  data.append('image', blob);
-
-  fetch('https://neto-api.herokuapp.com/photo-booth', {
-    body: data,
-    credentials: 'same-origin',
-    method: 'POST'
-  })
-  .then(result => {
-    if (200 <= result.status && result.status < 300) {
-      return result;
-    }
-    throw new Error(result.statusText);
-  })
-  .then(result => {
-    console.log(result);
-    target.style.display = 'none';
-  });
-}
-
-function dataUriToBlob(dataURI) {
-  const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-  const array = [];
-  const byteString = atob(dataURI.split(',')[1]);
-
-  for(let i = 0; i < byteString.length; i++) {
-    array.push(byteString.charCodeAt(i));
-  }
-
-  return new Blob([new Uint8Array(array)], { type: mimeString });
-}
+    })
+})
